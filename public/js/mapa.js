7755f591372e235e90c349ac9be8cf48a19a2994
szwedoman <szwedomaster@gmail.com>
2019-04-06 15:23:44 +0200

    new map design

--- public/js/mapa.js
+++ public/js/mapa.js
@@ -94,6 +94,18 @@ function initMap() {
     infoWindow = new google.maps.InfoWindow;
 
 // Try HTML5 geolocation.
+    localization();
+
+    var centerControlDiv = document.createElement('div');
+    var centerControl = new CenterControl(centerControlDiv, map);
+
+    centerControlDiv.index = 1;
+    centerControlDiv.style.margin='10px';
+    map.controls[google.maps.ControlPosition.TOP_RIGHT].push(centerControlDiv);
+
+}
+
+function localization() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
             var pos = {
@@ -120,4 +132,36 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
         'Error: The Geolocation service failed.' :
         'Error: Your browser doesn\'t support geolocation.');
     infoWindow.open(map);
+}
+
+function CenterControl(controlDiv, map) {
+
+    // Set CSS for the control border.
+    var controlUI = document.createElement('div');
+    controlUI.style.backgroundColor = 'rgba(255, 255, 255, .5)';
+    controlUI.style.border = '2px solid #fff';
+    controlUI.style.borderRadius = '3px';
+    controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+    controlUI.style.cursor = 'pointer';
+    controlUI.style.marginBottom = '22px';
+    controlUI.style.textAlign = 'left';
+    controlUI.title = 'Click to recenter the map';
+    controlUI.style.alignContent = 'left';
+    controlDiv.appendChild(controlUI);
+
+    // Set CSS for the control interior.
+    var controlText = document.createElement('div');
+    controlText.style.color = 'rgb(25,25,25)';
+    // controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+    // controlText.style.fontSize = '16px';
+    // controlText.style.lineHeight = '38px';
+    controlText.style.padding='5px';
+    controlText.innerHTML = '<img src="/img/map_svg/center2.svg" height="15px" width="15px">';
+    controlUI.appendChild(controlText);
+
+    // Setup the click event listeners: simply set the map to Chicago.
+    controlUI.addEventListener('click', function () {
+        localization();
+    });
+
 }
