2c96e42055089d6eb68fa3e97e832cc643f21f5c
Paweł Wangryn <pwangryn@op.pl>
2019-04-07 15:18:02 +0200

    Add notification chanel

--- public/app.js
+++ public/app.js
@@ -29,3 +29,19 @@ messaging.requestPermission()
     .catch(function (err) {
         console.log('Unable to get permission to notify.', err);
     });
+
+function subscribeTokenToTopic(token, topic) {
+    fetch('https://iid.googleapis.com/iid/v1/AAAAFtKD6Fo:APA91bHwDuXZDZ65ppcSYXRZIcsejARkFw0L6yZxS9pEDXV7VuqcxJb5pLg541FCjZpBG4pngukyNKGVS2G2wSbyUY4L9tl3Vlrqx-cSkKbIU9z3qdr3uGJjV5QQ3IbF1MuG6vsXdB0f/rel/topics/all', {
+        method: 'POST',
+        headers: new Headers({
+            Authorization:key='AIzaSyDDnKvi7GudJeLxswkpq4jITPYwSeek-W8'
+    })
+    }).then(response => {
+        if (response.status < 200 || response.status >= 400) {
+            throw 'Error subscribing to topic: '+response.status + ' - ' + response.text();
+        }
+        console.log('Subscribed to "all"');
+    }).catch(error => {
+        console.error(error);
+    })
+}
